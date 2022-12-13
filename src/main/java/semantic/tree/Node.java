885da88d79927f82bd6611441a94ae67275f2b60
bpuvaca <62650091+bpuvaca@users.noreply.github.com>
2022-12-13 12:40:37 +0100

    Implement parsing input into tree

--- src/main/java/semantic/tree/Node.java
+++ src/main/java/semantic/tree/Node.java
@@ -1,4 +1,17 @@
 package semantic.tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Node extends TreeElement{
+    private List<TreeElement> children = new ArrayList<>();
+
+    public List<TreeElement> getChildren() {
+        return children;
+    }
+
+    public Node(Node parent, String name) {
+        super(parent, name);
+        isLeaf = false;
+    }
 }
--- src/main/java/semantic/tree/TreeElement.java
+++ src/main/java/semantic/tree/TreeElement.java
@@ -1,4 +1,32 @@
 package semantic.tree;
 
 public abstract class TreeElement {
+    protected Node parent;
+    protected String name;
+    protected boolean isLeaf;
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isLeaf() {
+        return isLeaf;
+    }
+
+    public TreeElement(Node parent, String name) {
+        this.parent = parent;
+        this.name = name;
+    }
 }
